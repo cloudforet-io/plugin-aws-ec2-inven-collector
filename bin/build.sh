a6805cb3a8b037e7f4edb0aeea2a64f5c55196c1
dongyo <dkang@mz.co.kr>
2020-09-17 17:08:59 +0900

    Update Release Note

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,6 @@
 cd ..
 
 docker build -t pyengine/aws-ec2 . --no-cache
-docker tag pyengine/aws-ec2 pyengine/aws-ec2:1.3
-docker tag pyengine/aws-ec2 spaceone/aws-ec2:1.3
+docker tag pyengine/aws-ec2 pyengine/aws-ec2:1.3.1
+docker tag pyengine/aws-ec2 spaceone/aws-ec2:1.3.1
 

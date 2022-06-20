9fa74efb4896f87f82dcb75616540debf40380a3
Jihyung Song <bluese@megazone.com>
2022-06-20 16:44:06 +0900

    refactor: remove FILTER_FORMAT, update SUPPORTED_RESOURCE_TYPE

--- src/spaceone/inventory/conf/cloud_service_conf.py
+++ src/spaceone/inventory/conf/cloud_service_conf.py
@@ -1,61 +1,9 @@
 SUPPORTED_FEATURES = ['garbage_collection']
-SUPPORTED_RESOURCE_TYPE = ['inventory.Server', 'inventory.Region', 'inventory.CloudServiceType']
+SUPPORTED_RESOURCE_TYPE = ['inventory.CloudService', 'inventory.Region', 'inventory.CloudServiceType']
 SUPPORTED_SCHEDULES = ['hours']
 NUMBER_OF_CONCURRENT = 20
 DEFAULT_REGION = 'us-east-1'
-FILTER_FORMAT = [
-    {
-        'key': 'project_id',
-        'name': 'Project ID',
-        'type': 'str',
-        'resource_type': 'SERVER',
-        'search_key': 'identity.Project.project_id',
-        'change_rules': [{
-            'resource_key': 'data.compute.instance_id',
-            'change_key': 'instance_id'
-        }, {
-            'resource_key': 'data.compute.region',
-            'change_key': 'region_name'
-        }]
-    }, {
-        'key': 'collection_info.service_accounts',
-        'name': 'Service Account ID',
-        'type': 'str',
-        'resource_type': 'SERVER',
-        'search_key': 'identity.ServiceAccount.service_account_id',
-        'change_rules': [{
-            'resource_key': 'data.compute.instance_id',
-            'change_key': 'instance_id'
-        }, {
-            'resource_key': 'data.compute.region',
-            'change_key': 'region_name'
-        }]
-    }, {
-        'key': 'server_id',
-        'name': 'Server ID',
-        'type': 'list',
-        'resource_type': 'SERVER',
-        'search_key': 'inventory.Server.server_id',
-        'change_rules': [{
-            'resource_key': 'data.compute.instance_id',
-            'change_key': 'instance_id'
-        }, {
-            'resource_key': 'data.compute.region',
-            'change_key': 'region_name'
-        }]
-    }, {
-        'key': 'instance_id',
-        'name': 'Instance ID',
-        'type': 'list',
-        'resource_type': 'CUSTOM'
-    },
-    {
-        'key': 'region_name',
-        'name': 'Region',
-        'type': 'list',
-        'resource_type': 'CUSTOM'
-    }
-]
+FILTER_FORMAT = []
 
 REGION_INFO = {
     'us-east-1': {'name': 'US East (N. Virginia)',

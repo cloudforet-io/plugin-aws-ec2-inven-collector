91f0b1bdd022a9f5d9aa93998e37062eaaab6d70
Sooyoung98 <sooyoungkim@mz.co.kr>
2024-01-22 11:37:07 +0900

    fix: Added default value for OS Details in case of edge cases

--- src/spaceone/inventory/model/os.py
+++ src/spaceone/inventory/model/os.py
@@ -8,4 +8,4 @@ class OS(Model):
     details = StringType(default="")
     os_type = StringType(choices=('LINUX', 'WINDOWS'))
     # os_details = StringType(choices=('Linux/UNIX', 'Red Hat BYOL Linux', 'Red Hat Enterprise Linux', 'Red Hat Enterprise Linux with HA', 'Red Hat Enterprise Linux with SQL Server Standard and HA', 'Red Hat Enterprise Linux with SQL Server Enterprise and HA', 'Red Hat Enterprise Linux with SQL Server Standard', 'Red Hat Enterprise Linux with SQL Server Web', 'Red Hat Enterprise Linux with SQL Server Enterprise', 'SQL Server Enterprise', 'SQL Server Standard', 'SQL Server Web', 'SUSE Linux', 'Ubuntu Pro', 'Windows', 'Windows BYOL', 'Windows with SQL Server Enterprise', 'Windows with SQL Server Standard', 'Windows with SQL Server Web'))
-    os_details = StringType()
+    os_details = StringType(default="None")
